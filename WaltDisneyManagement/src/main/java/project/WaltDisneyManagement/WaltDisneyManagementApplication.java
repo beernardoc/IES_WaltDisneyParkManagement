84e620f49596a554edffbf663b42cccaf05610bb
JoaoCLS <joao.carlos.santos@ua.pt>
2023-11-09 12:29:01 +0000

    tudo

---
+++ WaltDisneyManagement/src/main/java/project/WaltDisneyManagement/WaltDisneyManagementApplication.java
@@ -0,0 +1,13 @@
+package project.WaltDisneyManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaltDisneyManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaltDisneyManagementApplication.class, args);
+	}
+
+}
---
+++ WaltDisneyManagement/src/test/java/project/WaltDisneyManagement/WaltDisneyManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package project.WaltDisneyManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaltDisneyManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

9d85deb65930312a9b1d8fe328fadbe8d2533b7c
beernardoc <bernardopinto@ua.pt>
2023-11-23 16:41:28 +0000

    new RabbitMq consumer using Beans

---
+++ WaltDisneyManagement/src/main/java/project/WaltDisneyManagement/rabbitMQ/Config.java
@@ -0,0 +1,40 @@
+package project.WaltDisneyManagement.rabbitMQ;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+
+@Configuration
+public class Config {
+
+    public static final String MagicKingdomQueue = "MagicKingdom";
+
+    public static final String EXCHANGE_NAME = "";
+
+    @Bean
+    Queue queue1() {
+        return new Queue(MagicKingdomQueue, true);
+    }
+
+
+    @Bean
+    DirectExchange exchange() { return new DirectExchange(EXCHANGE_NAME); }
+
+    @Bean
+    Binding binding1(@Qualifier("queue1") Queue queue, DirectExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(MagicKingdomQueue);
+    }
+
+    @Bean
+    public RabbitMQConsumer receiver() {
+        return new RabbitMQConsumer();
+    }
+
+
+}
--- WaltDisneyManagement/src/main/java/project/WaltDisneyManagement/rabbitMQ/RabbitMQConsumer.java
+++ WaltDisneyManagement/src/main/java/project/WaltDisneyManagement/rabbitMQ/RabbitMQConsumer.java
@@ -1,9 +1,11 @@
 package project.WaltDisneyManagement.rabbitMQ;
 
+import org.json.JSONException;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
+
 
 @Component
 public class RabbitMQConsumer {
@@ -11,16 +13,14 @@ public class RabbitMQConsumer {
     @Autowired
     private SimpMessagingTemplate messagingTemplate;
 
-    @Autowired
-    public RabbitMQConsumer(SimpMessagingTemplate messagingTemplate) {
-        this.messagingTemplate = messagingTemplate;
+
+    @RabbitListener(queues = Config.MagicKingdomQueue)
+    public void consumeMessageFromQueue(String message){
+
+
+
+        System.out.println("Message recieved from queue : " + message);
+        messagingTemplate.convertAndSend("/topic/atualizacao", message);
     }
 
-    @RabbitListener(queues = "MagicKingdom")
-    public void receberMensagem(String mensagem) {
-        System.out.println("[x] Recebido: " + mensagem);
-
-        // Enviar a mensagem para o cliente via WebSocket
-        //messagingTemplate.convertAndSend("/topic/atualizacao", mensagem);
-    }
 }

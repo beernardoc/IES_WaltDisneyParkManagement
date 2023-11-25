b8be7241e4035c051c675f48f6624b0e23d6e935
beernardoc <bernardopinto@ua.pt>
2023-11-25 01:05:58 +0000

    rabbitmq bug fix

--- WaltDisneyManagement/src/main/java/project/WaltDisneyManagement/rabbitMQ/RabbitMQConsumer.java
+++ WaltDisneyManagement/src/main/java/project/WaltDisneyManagement/rabbitMQ/RabbitMQConsumer.java
@@ -1,6 +1,6 @@
 package project.WaltDisneyManagement.rabbitMQ;
 
-import org.json.JSONException;
+
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;

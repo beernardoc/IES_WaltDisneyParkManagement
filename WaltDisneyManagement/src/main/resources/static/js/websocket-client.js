111ffa4e1a7f33b35caaffe47f58f139614f8fdf
Mariana Perna <marianaperna@ua.pt>
2023-11-26 18:59:35 +0000

    Dados dinamicos mas bugados

--- WaltDisneyManagement/src/main/resources/static/js/websocket-client.js
+++ WaltDisneyManagement/src/main/resources/static/js/websocket-client.js
@@ -1,5 +1,5 @@
-
 var stompClient = null;
+var jsonRecebido = null;
 
 function connect() {
     var socket = new SockJS('/websocket-endpoint');
@@ -7,21 +7,53 @@ function connect() {
     stompClient.connect({}, function (frame) {
         console.log('Connected: ' + frame);
         stompClient.subscribe('/topic/MagicKingdom', function (mensagem) {
+            jsonRecebido = JSON.parse(mensagem.body);
+            console.log('Dados recebidos:', jsonRecebido);
 
-            var jsonRecebido = JSON.parse(mensagem.body);
+            //document.getElementById('jsonRecebido').innerText = JSON.stringify(jsonRecebido);
+            //document.getElementById('railRoadVel').innerText = jsonRecebido["railRoad"]["velocity_kmh"];
 
-
-
-
-            document.getElementById('jsonRecebido').innerText = JSON.stringify(jsonRecebido); // ja como string
-
-            document.getElementById('railRoadVel').innerText = jsonRecebido.railRoad.velocity_kmh;
-
+            renderChart();
         });
-
     });
+}
+
+function renderChart() {
+    console.log('Renderizando gráfico');
+    // Certifique-se de que jsonRecebido é definido
+    if (jsonRecebido) {
+        new ApexCharts(document.querySelector("#barChart"), {
+            series: [{
+                data: [
+                    parseInt(jsonRecebido["railRoad"]["velocity_kmh"]),
+                    parseInt(jsonRecebido["railRoad"]["height_m"]),
+                    parseInt(jsonRecebido["railRoad"]["temperature"]),
+                    parseInt(jsonRecebido["railRoad"]["vibration"])
+                ]
+            }],
+            chart: {
+                type: 'bar',
+                height: 350
+            },
+            plotOptions: {
+                bar: {
+                    borderRadius: 4,
+                    horizontal: true,
+                }
+            },
+            dataLabels: {
+                enabled: false
+            },
+            xaxis: {
+                categories: ['Velocity km/h', 'Height m', 'Temperature ºC', 'Vibration'],
+            }
+        }).render();
+
+        console.log('ApexCharts version:', ApexCharts.version);
+    }
 }
 
 $(document).ready(function () {
     connect();
+    renderChart(); // Adicione esta chamada para garantir a renderização inicial
 });

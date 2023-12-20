a497c56446b5e7d38da0c6a89fccbdba82be6c06
beernardoc <bernardopinto@ua.pt>
2023-12-20 17:44:19 +0000

    sidebar para parking lot

--- WaltDisneyManagement/src/main/java/project/WaltDisneyManagement/controller/ParkCarsController.java
+++ WaltDisneyManagement/src/main/java/project/WaltDisneyManagement/controller/ParkCarsController.java
@@ -9,13 +9,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import project.WaltDisneyManagement.entity.Park;
 import project.WaltDisneyManagement.repository.EmployeeRepo;
 import project.WaltDisneyManagement.repository.ParkCarsRepo;
 import project.WaltDisneyManagement.entity.Attraction;
 import project.WaltDisneyManagement.entity.Employee;
 import project.WaltDisneyManagement.entity.ParkCars;
 import project.WaltDisneyManagement.service.EmployeeService;
+import project.WaltDisneyManagement.service.ParkCarsService;
+import project.WaltDisneyManagement.service.ParkService;
 
+import java.util.List;
 
 
 @Controller
@@ -27,6 +31,12 @@ public class ParkCarsController {
     @Autowired
     private EmployeeService employeeService;
 
+    @Autowired
+    private ParkService parkService;
+
+
+    @Autowired
+    private ParkCarsService parkCarsService;
 
     @GetMapping("/ParkCars/{parkCarsName}")
     public String park(Model model, @PathVariable("parkCarsName") String parkCarsName, HttpServletRequest request) {
@@ -41,11 +51,17 @@ public class ParkCarsController {
         model.addAttribute("role", employee.getRole());
         model.addAttribute("username", employee.getName());
 
-        ParkCars parkCars = ParkCarsRepo.findByName(parkCarsName);
-        model.addAttribute("status", parkCars.getStatus());
-        model.addAttribute("capacity", parkCars.getMaxcap());
-        model.addAttribute("atual", parkCars.getAtual());
-        model.addAttribute("parkCarName", parkCars.getName());
+        ParkCars parkCar = ParkCarsRepo.findByName(parkCarsName);
+        model.addAttribute("status", parkCar.getStatus());
+        model.addAttribute("capacity", parkCar.getMaxcap());
+        model.addAttribute("atual", parkCar.getAtual());
+        model.addAttribute("parkCarName", parkCar.getName());
+
+        List<Park> parks = parkService.findAll();
+        List<ParkCars> parkCars = parkCarsService.findAll();
+
+        model.addAttribute("parks", parks);
+        model.addAttribute("parkCars", parkCars);
 
         
 
